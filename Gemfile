a21cd07adcb0ecd6b36d948dc500b81640859c48
Mark Johnson <markj9@gmail.com>
2014-08-27 13:38:52 -0400

    Renamed first specs.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+gem 'vcr'
+gem 'webmock'
+gem 'pry'
+gem 'rdf'
+gem 'linkeddata'
+
+group :test do
+  gem 'rspec'
+end
--- spec/integration/economic_data_courier_spec.rb
+++ spec/integration/economic_data_courier_spec.rb
@@ -4,6 +4,12 @@
 # 4.  Persist all the economic indicators for each country.
 require 'spec_helper.rb'
 
-describe DatastreamDataPull do
+describe EconomicDataCourier do
+  describe "retrieving datastream data" do
+    it "should take a path to a file containing mneumonics for user economic lists" do
+      subject = EconomicDataCourier.new datastream_list_path: '../input/datastream_lists.yml'
+      expect(subject.datastream_user_lists).not_to be_empty
+    end
+  end
 end
 
